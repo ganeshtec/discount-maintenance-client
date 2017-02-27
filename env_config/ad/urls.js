f3c1e8d0d1227b04fa190a4c3aae28952e0b5b19
Lewis <sxl2958@d25rq0zxgq17im.ssc.homedepot.com>
2017-02-27 13:56:40 -0500

    Add ext to url for AD WS

--- env_config/ad/urls.js
+++ env_config/ad/urls.js
@@ -3,7 +3,7 @@ app.factory('URL_CONFIG', [function() {
     var _construct = function URL_CONFIG() {
         var data = {
             "dashboardUiUrl": "https://promotionsadmin-ad.apps-np.homedepot.com",
-            "serviceUrl": "https://promotionsws-ad.apps-np.homedepot.com/v1",
+            "serviceUrl": "https://promotionsws-ext-ad.apps-np.homedepot.com/v1",
             "authorizeUrl": "https://promotionsws-ad.apps-np.homedepot.com/v1/authorization/isAuthorized.json?userid=",
             "endecaUrl": "https://origin-thdws.gcp-stage.homedepot.com/TA2/facet"
         };
