e3ba8fa056f949426019bdcc0f507c08e8508d33
Fitsum <fxt8693@homedepot.com>
2017-03-01 16:04:37 -0500

    urls updated according to dev urls

--- env_config/qa/urls.js
+++ env_config/qa/urls.js
@@ -2,8 +2,8 @@
 app.factory('URL_CONFIG', [function() {
     var _construct = function URL_CONFIG() {
         var data = {
-            "dashboardUiUrl": "https://promotionsadmin-qa.apps-np.homedepot.com",
-            "serviceUrl": "https://promotionsadmin-ext-qa.apps-np.homedepot.com/v1",
+            "dashboardUiUrl": "https://promotionsadmin-qa.apps-np.homedepot.com",         
+            "serviceUrl" : "https://promotionsws-ext-qa.apps-np.homedepot.com/v1",
             //"authorizeUrl" : "https://promotionsws-qa.apps-np.homedepot.com/v1/authorization/isAuthorized.json?userid=",
             "authorizeUrl" : "https://promotionsws-ext-qa.apps-np.homedepot.com/v1/authorization/isAuthorized/",
             "endecaUrl": "https://origin-thdws.gcp-stage.homedepot.com/TA2/facet"
