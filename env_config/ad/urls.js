cbd860ff24804fdaa033163d1f344671fbcc6cba
Fitsum <fxt8693@homedepot.com>
2017-03-01 14:13:47 -0500

    Urls updated according to dashboard URL

--- env_config/ad/urls.js
+++ env_config/ad/urls.js
@@ -2,7 +2,7 @@
 app.factory('URL_CONFIG', [function() {
     var _construct = function URL_CONFIG() {
         var data = {
-            "dashboardUiUrl": "https://promotionsadmin-ext-ad.apps-np.homedepot.com",            
+            "dashboardUiUrl": "https://promotionsadmin-ad.apps-np.homedepot.com",
             "serviceUrl": "https://promotionsws-ext-ad.apps-np.homedepot.com/v1",
             "authorizeUrl": "https://promotionsws-ext-ad.apps-np.homedepot.com/v1/authorization/isAuthorized/",
             "endecaUrl": "https://origin-thdws.gcp-stage.homedepot.com/TA2/facet"
--- env_config/dev/urls.js
+++ env_config/dev/urls.js
@@ -2,7 +2,7 @@
 app.factory('URL_CONFIG', [function(){	
 	var _construct = function URL_CONFIG(){
 		var data = {
-			"dashboardUiUrl": "https://promotionsadmin-ext-qa.apps-np.homedepot.com",
+			"dashboardUiUrl": "https://promotionsadmin-qa.apps-np.homedepot.com",
 			"serviceUrl" : "https://promotionsws-ext-qa.apps-np.homedepot.com/v1",
 			//"authorizeUrl" : "https://promotionsws-ext-qa.apps-np.homedepot.com/v1/authorization/isAuthorized.json?userid=",
 	    	"authorizeUrl": "https://promotionsws-ext-qa.apps-np.homedepot.com/v1/authorization/isAuthorized/",
