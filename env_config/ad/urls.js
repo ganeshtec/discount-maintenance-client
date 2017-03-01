1403cac0b39005f7c911a440ae991fd2276f4484
Fitsum <fxt8693@homedepot.com>
2017-03-01 12:27:09 -0500

    reverted back urls

--- env_config/ad/urls.js
+++ env_config/ad/urls.js
@@ -2,12 +2,9 @@
 app.factory('URL_CONFIG', [function() {
     var _construct = function URL_CONFIG() {
         var data = {
-            "dashboardUiUrl": "http://localhost.homedepot.com",
-            //"serviceUrl": "https://promotionsws-qa.apps-np.homedepot.com/v1",
-            //"serviceUrl": "https://promotionswssku-qa.apps-np.homedepot.com/v1",
-            "serviceUrl": "http://http://localhost.homedepot.com:8090/v1",
-            "authorizeUrl": "http://localhost.homedepot.com:8090/v1/authorization/isAuthorized/"
-           //"authorizeUrl" : "https://promotionsws-qa.apps-np.homedepot.com/v1/authorization/isAuthorized.json?userid=",
+            "dashboardUiUrl": "https://promotionsadmin-ext-ad.apps-np.homedepot.com",            
+            "serviceUrl": "https://promotionsws-ext-ad.apps-np.homedepot.com/v1",
+            "authorizeUrl": "https://promotionsws-ext-ad.apps-np.homedepot.com/v1/authorization/isAuthorized/",
             "endecaUrl": "https://origin-thdws.gcp-stage.homedepot.com/TA2/facet"
         };
         return data;
