5673bd5333dc79cf21a3ed75c19b81d337889782
Fitsum <fxt8693@homedepot.com>
2017-03-03 10:38:49 -0500

    Capybara Changes for Roles

--- capybaraTests/spec/journies/customer_segments_spec.rb
+++ capybaraTests/spec/journies/customer_segments_spec.rb
@@ -21,18 +21,23 @@ describe 'MFA makes a customer segment discount that is persisted', type: :featu
 			)
 	end
 
-	it 'provides the MFA-created customer segment discount to the Promotion_Maintenance_UI' do
-		create_cust_segment_discount(promotion)
+    it 'provides the MFA-created customer segment discount to the Promotion_Maintenance_UI for STOREUSER' do
+		create_cust_segment_discount_store(promotion)
+	end
+	
+	it 'provides the MFA-created customer segment discount to the Promotion_Maintenance_UI for ONLINEUSER' do
+		create_cust_segment_discount_online(promotion)
 	end
 
-	it 'edits the MFA-created customer segment discount to the Promotion_Maintenance_UI' do
+	it 'edits the MFA-created customer segment discount to the Promotion_Maintenance_UI for ONLINEUSER' do
 		edit_cust_segment_discount()
 	end
 	
+	
 end
 
-def create_cust_segment_discount(promotion)
-	sign_in
+def create_cust_segment_discount_store(promotion)
+	store_sign_in
 	expect(page).to have_css('.md-button', text: 'CREATE NEW PROMOTION')
 	puts "SIGN IN COMPLETED WITHOUT ERROR"
     
@@ -40,44 +45,35 @@ def create_cust_segment_discount(promotion)
 	puts "Create New Promotion SUCCESS" 
 	
 	fill_in 'name', with: promotion.name
-	puts "PROMOTION NAME ADDED SUCCESSFULLY"
 	select(promotion.promotion_type, :from => 'promotype')
-    puts "PROMOTION Type ADDED SUCCESSFULLY"
-	click_button 'Next'
-    puts "PROMOTION Next Button on Tab-1"
+    click_button 'Next'
+    
     
     select(promotion.customer_segment, :from => 'segment')
     puts "PROMOTION Customer Segment ADDED SUCCESSFULLY"
 
     puts "expect statement SUCCESSFULLY"
     choose("purchaseoption2")
-    puts "SELECTED ITEMS/SKUS"
+    
 
     fill_in 'Search and Add Item Sku Number', with: promotion.item_sku
     puts "PROMOTION inclusion Item Sku After Search"
     click_on 'Search'
     click_button 'Next'
-    puts "Location Tab Reached SUCCESSFULLY"
-
-
+    
     fill_in 'Search and Add Store Number', with: promotion.store_location
     click_on 'Search'
 
-	puts "Store added SUCCESSFULLY"
-
-    click_button 'Next'
+	click_button 'Next'
 
     fill_in 'minamount', with: promotion.rewards_quantity
-    puts " Min Quantity added SUCCESSFULLY"
     fill_in 'value', with: promotion.amount_off
-	puts " Amount off added SUCCESSFULLY"
-
-    click_button 'Next'
-
-    fill_in 'shortdesc', with: promotion.desc_short
-	fill_in 'longdesc', with: promotion.desc_long
-	puts " S & L Description added SUCCESSFULLY"
 	click_button 'Next'
+
+    #fill_in 'shortdesc', with: promotion.desc_short
+	#fill_in 'longdesc', with: promotion.desc_long
+	#puts " S & L Description added SUCCESSFULLY"
+	#click_button 'Next'
 
 	click_button 'Next'
 
@@ -86,13 +82,61 @@ def create_cust_segment_discount(promotion)
 	click_on 'Preview & Submit'
 	click_on 'Submit'
 	sleep(12)
-	puts promotion.name + " Submit process begin"
+	puts promotion.name + " Submit process begin for Store User"
+	expect(page).to have_content promotion.name
+	puts "Discount Found on Discount List"
+	
+end
+
+ def create_cust_segment_discount_online(promotion)
+	online_sign_in
+	expect(page).to have_css('.md-button', text: 'CREATE NEW PROMOTION')
+	puts "SIGN IN COMPLETED WITHOUT ERROR"
+    
+    click_on 'Create New Promotion'
+	puts "Create New Promotion SUCCESS" 
+	
+	fill_in 'name', with: promotion.name
+	select(promotion.promotion_type, :from => 'promotype')
+    click_button 'Next'
+    
+    
+    select(promotion.customer_segment, :from => 'segment')
+    choose("purchaseoption2")
+    fill_in 'Search and Add Item Sku Number', with: promotion.item_sku
+    click_on 'Search'
+    click_button 'Next'
+    
+    fill_in 'Search and Add Store Number', with: promotion.store_location
+    click_on 'Search'
+
+	click_button 'Next'
+
+    fill_in 'minamount', with: promotion.rewards_quantity
+    fill_in 'value', with: promotion.amount_off
+	
+    click_button 'Next'
+
+    fill_in 'shortdesc', with: promotion.desc_short
+	fill_in 'longdesc', with: promotion.desc_long
+	puts " S & L Description added SUCCESSFULLY"
+	
+	click_button 'Next'
+	click_button 'Next'
+
+	fill_in 'start', with: promotion.start_date 
+    fill_in 'end', with: promotion.end_date 
+	click_on 'Preview & Submit'
+	click_on 'Submit'
+	sleep(12)
+	puts promotion.name + " Submit process begin for Online User"
 	expect(page).to have_content promotion.name
 	puts "Discount Found on Discount List"
 end
 
+
 def edit_cust_segment_discount()
-	sign_in
+	online_sign_in
 	
 	page.find('tr', :text => 'Active', :match => :first)
 	within('tr', :text => 'Active', :match => :first) do
@@ -106,54 +150,42 @@ def edit_cust_segment_discount()
 	sleep(5)
 
 	fill_in 'name', with: promotion.name
-	puts "PROMOTION NAME ADDED SUCCESSFULLY"
 	select(promotion.promotion_type, :from => 'promotype')
-    puts "PROMOTION Type ADDED SUCCESSFULLY"
-	click_button 'Next'
-    puts "PROMOTION Next Button on Tab-1"
-
+    click_button 'Next'
     select(promotion.customer_segment, :from => 'segment')
-    puts "PROMOTION Customer Segment ADDED SUCCESSFULLY"
+    
 
-    puts "expect statement SUCCESSFULLY"
     choose("purchaseoption2")
-    puts "SELECTED ITEMS/SKUS"
-
+    
     fill_in 'Search and Add Item Sku Number', with: promotion.item_sku
-    puts "PROMOTION inclusion Item Sku After Search"
     click_on 'Search'
     click_button 'Next'
-    puts "Location Tab Reached SUCCESSFULLY"
-
 
     fill_in 'Search and Add Store Number', with: promotion.store_location
     click_on 'Search'
 
-	puts "Store added SUCCESSFULLY"
-
     click_button 'Next'
 
     fill_in 'minamount', with: promotion.rewards_quantity
-    puts " Min Quantity added SUCCESSFULLY"
     fill_in 'value', with: promotion.amount_off
-	puts " Amount off added SUCCESSFULLY"
+	
 
     click_button 'Next'
 
     fill_in 'shortdesc', with: promotion.desc_short
 	fill_in 'longdesc', with: promotion.desc_long
-	puts " S & L Description added SUCCESSFULLY"
+	
 	click_button 'Next'
-
 	click_button 'Next'
 
 	fill_in 'start', with: promotion.start_date 
     fill_in 'end', with: promotion.end_date 
 	click_on 'Preview & Submit'
 	click_on 'Submit'
-	puts promotion.name + " Submit process begin"
+	puts promotion.name + " Successfully Edited for Online User"
 	expect(page).to have_content promotion.name
 	puts "Discount Found on Discount List"
 	sleep(12)
 
-end
+end 
+
