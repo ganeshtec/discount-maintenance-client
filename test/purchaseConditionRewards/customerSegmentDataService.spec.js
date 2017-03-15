219f5a3428787d14331c10748ad13b6351cb0ea5
Justin Doak <pair+jdoak@pivotallabs.com>
2017-03-15 11:02:37 -0400

    WIP eslint - work on linting problems in tests

--- test/purchaseConditionRewards/customerSegmentDataService.spec.js
+++ test/purchaseConditionRewards/customerSegmentDataService.spec.js
@@ -1,21 +1,16 @@
-describe('checking Customer Segment Data Service for 200 response code', function(){
-    
-    var 
-    $rootScope,
-    $scope, 
-    location,
-    $httpBackend;
-    
+/* eslint-disable no-unused-vars */
+describe('checking Customer Segment Data Service for 200 response code', function () {
+    var $rootScope,
+        $scope,
+        location,
+        $httpBackend;
 
-      it('Test a Data Service call ', inject(function($httpBackend,$http) {
-     
-           var url = "http://promotionsadminsku.apps-np.homedepot.com/v1/customersegment/segments";
-          
-             $httpBackend.when('POST', url, function()
-                             {
-                            return true;
-                           }).respond(200, true);
-                      
-                  }));
-    
+
+    it('Test a Data Service call ', inject(function ($httpBackend) {
+
+        var url = 'http://promotionsadminsku.apps-np.homedepot.com/v1/customersegment/segments';
+        $httpBackend.when('POST', url, function () {
+            return true;
+        }).respond(200, true);
+    }));
 });
--- test/tableSort/tableSort.directive.spec.js
+++ test/tableSort/tableSort.directive.spec.js
@@ -1,19 +1,20 @@
-describe('Unit testing tableSort.directive.spec.js', function() {
-  var $compile,
-      $rootScope,
-      $scope,
-      element;
+/* eslint-disable no-unused-vars */
+describe('Unit testing tableSort.directive.spec.js', function () {
+    var $compile,
+        $rootScope,
+        $scope,
+        element;
 
   // Load the myApp module, which contains the directive
-  beforeEach(module('app'));
+    beforeEach(module('app'));
 
   // Store references to $rootScope and $compile
   // so they are available to all tests in this describe block
-  beforeEach(inject(function(_$compile_, _$rootScope_){
-    // The injector unwraps the underscores (_) from around the parameter names when matching
-    $compile = _$compile_;
-    $rootScope = _$rootScope_;
-    $scope = $rootScope.$new();
-  }));
+    beforeEach(inject(function (_$compile_, _$rootScope_) {
+      // The injector unwraps the underscores (_) from around the parameter names when matching
+        $compile = _$compile_;
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+    }));
 
 });
