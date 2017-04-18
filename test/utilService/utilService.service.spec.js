9cfd3d4311bb6180a722f50ab8e7bb5efe1325ee
Justin Doak and William Tabor <pair+jdoak+wtabor@pivotallabs.com>
2017-04-18 11:37:19 -0400

    WIP Validations

--- test/utilService/utilService.service.spec.js
+++ test/utilService/utilService.service.spec.js
@@ -19,7 +19,13 @@ describe('utilService', function() {
     }));
     
     it('determines if a promotion is active', function() {
-        utilService.isPromotionActive();
+        var activePromo = {}
+        activePromo.startDt = "2017-04-18 00:00:00";
+        expect(utilService.isPromotionActive(activePromo)).toBe(true);
+
+        var inactivePromo = {}
+        inactivePromo.startDt = "";
+        expect(utilService.isPromotionActive(inactivePromo)).toBe(false);
     });
 
 });
