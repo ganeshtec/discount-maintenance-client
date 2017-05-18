cccabfb07f3489830338cef59e42a385243c7bdc
Thangapandian <GANESAN_THANGAPANDIAN@homedepot.com>
2017-05-17 21:21:34 -0400

    warning when attributes exceed certain parameters-Updates[#140936217]

---
+++ src/js/directives/toolTipperWarning/toolTipperWarning.js
@@ -0,0 +1,18 @@
+app.component('toolTipperWarning', {
+    templateUrl: 'toolTipperWarning.html',
+    bindings: {
+        showToolTip: '<'
+    },
+    transclude: true,
+    controller: ToolTipperWarningController
+});
+
+//PromoLabelsController.$inject = ['validationService'];
+
+function ToolTipperWarningController() {
+
+
+
+  
+
+}
