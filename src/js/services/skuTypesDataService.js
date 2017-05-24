6761012ab70e975d46014aff48dc9d23d6e04805
John Tippabattuni & Don Bailey <pair+jtippabattuni+dbailey@pivotallabs.com>
2017-05-24 17:40:23 -0400

    Added SKUTypes Data Service. SkuTypeModal is still work in progress [#141317387]
    	new file:   src/js/components/skuTypeModal/skuTypeModal.html
    	new file:   src/js/components/skuTypeModal/skuTypeModal.js
    	new file:   src/js/services/skuTypesDataService.js
    	new file:   test/skuTypesService/skuTypesDataService.service.spec.js

---
+++ src/js/services/skuTypesDataService.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+app.service('skuTypesDataService', ['$http', '$q', 'dataService',
+    function ($http, $q, dataService) {
+        var publicApi = {};
+        var skuTypes;
+        publicApi.fetchSkuTypes = function (data) {
+            var config = {
+                    method: 'GET',
+                    url: '/skutypes',
+                },
+                result = $q.defer();
+            if (skuTypes != undefined) {
+                result.resolve(skuTypes);
+            } else {
+                dataService.httpRequest(config).then(
+                    function (response) {
+                        skuTypes = response.data;
+                        result.resolve(skuTypes);
+                    },
+                    function (error) {
+                        result.reject(error);
+                    });             
+            }
+            return result.promise;
+        }
+        return publicApi;
+    }
+]);
