cc22b19c0bcd60e52a956384b3bdae24fe7a8cc2
Smith, Coredelle <COREDELLE_SMITH@homedepot.com>
2017-06-07 13:25:35 -0400

    update urls.js

--- env_config/dev/urls.js
+++ env_config/dev/urls.js
@@ -2,9 +2,8 @@ app.factory('URL_CONFIG', [function(){
     var _construct = function URL_CONFIG(){
         var data = {
             "dashboardUiUrl": "https://promotionsadmin-qa.apps-np.homedepot.com",
-            //"serviceUrl" : "https://promotionsws-ext-qa.apps-np.homedepot.com/v1",
-            "serviceUrl" : "https://promotionsws-ext-ad.apps-np.homedepot.com/v1",
-            "authorizeUrl": "https://promotionsws-ext-ad.apps-np.homedepot.com/v1/authorization/isAuthorized/",
+            "serviceUrl" : "https://promotionsws-ext-qa.apps-np.homedepot.com/v1",
+            "authorizeUrl": "https://promotionsws-ext-qa.apps-np.homedepot.com/v1/authorization/isAuthorized/",
             "endecaUrl": "https://origin-thdws.gcp-stage.homedepot.com/TA2/facet"
         };
         return data;
