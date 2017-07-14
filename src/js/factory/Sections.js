63175dacdc001213a759e280a924dff9e229cd30
John Tippabattuni and Ryan Ramdehol <pair+jtippabattuni+rramdehol@pivotallabs.com>
2017-07-14 12:49:24 -0400

    Updates to hide location and labels sections for DCMs/online users
    [#148786319]

--- src/js/factory/Sections.js
+++ src/js/factory/Sections.js
@@ -17,11 +17,11 @@ app.factory('SECTIONS', ['ALLOWED_PERMISSION_IDS', function(ALLOWED_PERMISSION_I
             return [
                 {name:'Discount Properties',isActive:true,shouldDisplay:true, link: 'promotion-properties', icon: 'fa-gear' },
                 {name:'Purchase Conditions',isActive:false,shouldDisplay:true, link: 'promotion-condition-rewards', icon: 'fa-money'},
-                {name:'Location',isActive:false,shouldDisplay:true, link: 'promotion-location', icon: 'fa-globe'},
+                {name:'Location',isActive:false,shouldDisplay:false, link: 'promotion-location', icon: 'fa-globe'},
                 {name:'Rewards',isActive:false,shouldDisplay:true, link: 'promotion-rewards', icon: 'fa-gift'},
                 {name:'Descriptions',isActive:false,shouldDisplay:true, link: 'promotion-descriptions', icon: 'fa-pencil-square-o'},
                 {name:'Redemption Limits',isActive:false,shouldDisplay:true, link: 'promotion-redemtion-limits', icon: 'fa-tachometer' },
-                {name:'Labels',isActive:false,shouldDisplay:true, link: 'promo-Labels', icon: 'fa-tag' },
+                {name:'Labels',isActive:false,shouldDisplay:false, link: 'promo-Labels', icon: 'fa-tag' },
                 {name:'Schedule',isActive:false,shouldDisplay:true, link: 'promotion-schedule', icon: 'fa-calendar'}
             ];
         } else {
