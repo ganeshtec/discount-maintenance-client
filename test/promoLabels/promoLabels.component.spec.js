ccdd080d18ad0123f2b74819058fe6694bb2c827
Ganesan Thangapandian and Ryan Ramdehol <pair+gthangapandian+rramdehol@pivotallabs.com>
2017-07-20 16:52:28 -0400

    Unit Test updated for printLabel
    [#149232697]

--- test/promoLabels/promoLabels.component.spec.js
+++ test/promoLabels/promoLabels.component.spec.js
@@ -1,29 +1,42 @@
 describe('promoLabels', function () {
-    var $componentController;
+    var $componentController,
+    $rootScope,
+    $compile,
+    $httpBackend,
+    promotionDataService;
     
     // Load the myApp module, which contains the directive
     beforeEach(module('app'));
 
-    beforeEach(inject(function(_$componentController_) {
+    beforeEach(inject(function(_$componentController_,_$rootScope_,_$compile_,_$httpBackend_,_promotionDataService_) {
     $componentController = _$componentController_;
+    $rootScope = _$rootScope_;
+    $compile = _$compile_;
+    $httpBackend = _$httpBackend_;
+    promotionDataService = _promotionDataService_;
           
     ctrl = $componentController('promoLabels',null, {
-        data: {printLabel:true, labelText:""} 
+       data: {} 
          });
+     var leadTime = $httpBackend.when('GET', '/labels/leadTime')
+                            .respond(200,3);      
 
   }));
 
-  // These tests don't seem to assert anything within the controller. Commenting out for now.
+ // Print Label component coverage been handled in adminPromotionForm.directive.spec.js
 
-    // it('to check printLabel checkbox checked by default', function () {
-       
-    //     expect(ctrl.data.printLabel).toBe(true);        
-    // });
-
-    // it('to check printLabel Text is disabled when checkbox is unchecked', function () {
-
-    //     expect(ctrl.data.labelText).toBe("");
-    // });
-
+    it('to check printLabel checkbox checked by default', function () {
+      //ctrl.data.promoSubTypeCd ='ProductLevelPerItemPercentDiscountMSB';
+      var data = {};
+      $rootScope.data = data;
+      var element = $compile("<promo-labels data='data' ></promo-labels>")($rootScope);
+      $rootScope.$digest();
+      ctrl.data.printLabel=true; 
+      ctrl.data.labelText='';
+      
+      expect(ctrl.data.printLabel).toBe(true);
+      expect(ctrl.data.labelText).toEqual("");
+      expect(ctrl.data.labelText.length).toBe(0);        
+    });
    
 });
