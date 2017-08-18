4b1c367d1ba883ba0f192b8118ebf16da5063b3c
Ganesan Thangapandian and Nageswararao Kanchepu <pair+gthangapandian+nkanchepu@pivotallabs.com>
2017-08-18 13:32:38 -0400

    Removed all References for Endeca in Urls.js [#150294719]

--- env_config/ad/urls.js
+++ env_config/ad/urls.js
@@ -4,7 +4,6 @@ app.factory('URL_CONFIG', [function() {
             "dashboardUiUrl": "https://promotionsadmin-ad.apps-np.homedepot.com",
             "serviceUrl": "https://promotionsws-ext-ad.apps-np.homedepot.com/v1",
             "authorizeUrl": "https://promotionsws-ext-ad.apps-np.homedepot.com/v1/authorization/isAuthorized/",
-            "endecaUrl": "https://origin-thdws.gcp-stage.homedepot.com/TA2/facet"
         };
         return data;
     };
--- env_config/qa/urls.js
+++ env_config/qa/urls.js
@@ -4,7 +4,6 @@ app.factory('URL_CONFIG', [function() {
             "dashboardUiUrl": "https://promotionsadmin-qa.apps-np.homedepot.com",         
             "serviceUrl" : "https://promotionsws-ext-qa.apps-np.homedepot.com/v1",
             "authorizeUrl" : "https://promotionsws-ext-qa.apps-np.homedepot.com/v1/authorization/isAuthorized/",
-            "endecaUrl": "https://origin-thdws.gcp-stage.homedepot.com/TA2/facet"
 
         };
         return data;
