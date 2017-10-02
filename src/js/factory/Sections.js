5711d46218fb4d34478552f1030022d32c140c8c
Spencer Lewis <sxl2958>
2017-10-02 09:35:54 -0400

    update default for show labels section

--- src/js/factory/Sections.js
+++ src/js/factory/Sections.js
@@ -32,7 +32,7 @@ app.factory('SECTIONS', ['ALLOWED_PERMISSION_IDS', function(ALLOWED_PERMISSION_I
                 {name:'Rewards',isActive:false,shouldDisplay:true, link: 'promotion-rewards', icon: 'fa-gift'},
                 {name:'Descriptions',isActive:false,shouldDisplay:true, link: 'promotion-descriptions', icon: 'fa-pencil-square-o'},
                 {name:'Redemption Limits',isActive:false,shouldDisplay:true, link: 'promotion-redemtion-limits', icon: 'fa-tachometer' },
-                {name:'Labels',isActive:false,shouldDisplay:true, link: 'promo-Labels', icon: 'fa-tag' },
+                {name:'Labels',isActive:false,shouldDisplay:false, link: 'promo-Labels', icon: 'fa-tag' },
                 {name:'Schedule',isActive:false,shouldDisplay:true, link: 'promotion-schedule', icon: 'fa-calendar'}
             ];
         }
