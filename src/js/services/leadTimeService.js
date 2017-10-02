9f91fd92abe0a2777633584edc6ba01d5efaa0ba
Spencer Lewis <sxl2958>
2017-10-02 09:16:30 -0400

    Resolve linter errors

--- src/js/services/leadTimeService.js
+++ src/js/services/leadTimeService.js
@@ -24,22 +24,22 @@ app.service('leadTimeService', ['$http', '$q', 'dataService',
 
         publicApi.fetchLabelToggle = function (data) {
             
-                        var config = {
-                                method: 'GET',
-                                url: '/labels/showLabelSection',
-                            },
-                            result = $q.defer();
-            
-                        dataService.httpRequest(config).then(
-                            function (response) {
-                                result.resolve(response.data);
-                            },
-                            function (error) {
-                                result.reject(error);
-                            });
-            
-                        return result.promise;
-                    }
+            var config = {
+                    method: 'GET',
+                    url: '/labels/showLabelSection',
+                },
+                result = $q.defer();
+
+            dataService.httpRequest(config).then(
+                function (response) {
+                    result.resolve(response.data);
+                },
+                function (error) {
+                    result.reject(error);
+                });
+
+            return result.promise;
+        }
         return publicApi;
     }
 ]);
