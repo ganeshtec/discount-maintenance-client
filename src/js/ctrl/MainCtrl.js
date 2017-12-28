398b5faba035f8b5ef05de064f11eda3dc9d1507
Beaver <bxb2054@d25sp182gq17im.amer.homedepot.com>
2017-12-28 13:34:24 -0500

    [138360147] fix preserved search to accomodate for refactor

--- src/js/ctrl/MainCtrl.js
+++ src/js/ctrl/MainCtrl.js
@@ -1,6 +1,6 @@
 // Main Controller for root scope
-app.controller('MainCtrl', ['$scope', '$location', '$cookies', 'DataFactory', 'SECTIONS', 'promotionDataService', 'loginService', 'OverlayConfigFactory',
-    function ($scope, $location, $cookies, DataFactory, SECTIONS, promotionDataService, loginService, OverlayConfigFactory) {
+app.controller('MainCtrl', ['$rootScope', '$scope', '$location', '$cookies', 'DataFactory', 'SECTIONS', 'promotionDataService', 'loginService', 'OverlayConfigFactory',
+    function ($rootScope, $scope, $location, $cookies, DataFactory, SECTIONS, promotionDataService, loginService, OverlayConfigFactory) {
         $scope.messageModal = DataFactory.messageModal;
         $scope.username = '';
         $scope.userPermissions = '';
@@ -67,7 +67,7 @@ app.controller('MainCtrl', ['$scope', '$location', '$cookies', 'DataFactory', 'S
             $cookies.put('logout', 'true', {
                 'domain': '.homedepot.com'
             });
-            
+            $rootScope.searchParams = null;
             $scope.username = '';
             $scope.userPermissions = '';
             $scope.userRoleSelected.id = null;
