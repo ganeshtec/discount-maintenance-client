9a12c6052ba980eb2a13fc6786474bdb3907d436
Patrick Woods and Spandan Amanaganti <pair+Patrick+Spandan@pivotallabs.com>
2018-01-24 11:59:24 -0500

    Change gulp to compile SASS rather than CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var input = {
             'src/js/**/*.js'
         ],
         'source_css': [
-            'src/**/*.css'
+            'src/**/*.scss'
         ],
         'vendor_css': [
             'node_modules/angular-material/angular-material.min.css',
