c39952aff232c737210c713c8e8c07a56910acf9
Patrick Woods and Spandan Amanaganti <pair+Patrick+Spandan@pivotallabs.com>
2018-01-25 14:39:02 -0500

    Rename properties and purchase conditions.

--- src/js/factory/Sections.js
+++ src/js/factory/Sections.js
@@ -4,8 +4,8 @@ app.factory('SECTIONS', ['ALLOWED_PERMISSION_IDS', function(ALLOWED_PERMISSION_I
         var allowedPermissionIDs=ALLOWED_PERMISSION_IDS();
         if (userType == allowedPermissionIDs.STORE) {
             return [
-                {name:'Discount Properties',isActive:true,shouldDisplay:true, link: 'promotion-properties', icon: 'fa-gear' },
-                {name:'Purchase Conditions',isActive:false,shouldDisplay:true, link: 'promotion-condition-rewards', icon: 'fa-money'},
+                {name:'Properties',isActive:true,shouldDisplay:true, link: 'promotion-properties', icon: 'fa-gear' },
+                {name:'Qualifiers',isActive:false,shouldDisplay:true, link: 'promotion-condition-rewards', icon: 'fa-money'},
                 {name:'Location',isActive:false,shouldDisplay:true, link: 'promotion-location', icon: 'fa-globe'},
                 {name:'Rewards',isActive:false,shouldDisplay:true, link: 'promotion-rewards', icon: 'fa-gift'},
                 {name:'Descriptions',isActive:false,shouldDisplay:false, link: 'promotion-descriptions', icon: 'fa-pencil-square-o'},
@@ -15,8 +15,8 @@ app.factory('SECTIONS', ['ALLOWED_PERMISSION_IDS', function(ALLOWED_PERMISSION_I
             ];
         } else if (userType == allowedPermissionIDs.ONLINE) {
             return [
-                {name:'Discount Properties',isActive:true,shouldDisplay:true, link: 'promotion-properties', icon: 'fa-gear' },
-                {name:'Purchase Conditions',isActive:false,shouldDisplay:true, link: 'promotion-condition-rewards', icon: 'fa-money'},
+                {name:'Properties',isActive:true,shouldDisplay:true, link: 'promotion-properties', icon: 'fa-gear' },
+                {name:'Qualifiers',isActive:false,shouldDisplay:true, link: 'promotion-condition-rewards', icon: 'fa-money'},
                 {name:'Location',isActive:false,shouldDisplay:false, link: 'promotion-location', icon: 'fa-globe'},
                 {name:'Rewards',isActive:false,shouldDisplay:true, link: 'promotion-rewards', icon: 'fa-gift'},
                 {name:'Descriptions',isActive:false,shouldDisplay:true, link: 'promotion-descriptions', icon: 'fa-pencil-square-o'},
@@ -26,8 +26,8 @@ app.factory('SECTIONS', ['ALLOWED_PERMISSION_IDS', function(ALLOWED_PERMISSION_I
             ];
         } else {
             return [
-                {name:'Discount Properties',isActive:true,shouldDisplay:true, link: 'promotion-properties', icon: 'fa-gear' },
-                {name:'Purchase Conditions',isActive:false,shouldDisplay:true, link: 'promotion-condition-rewards', icon: 'fa-money'},
+                {name:'Properties',isActive:true,shouldDisplay:true, link: 'promotion-properties', icon: 'fa-gear' },
+                {name:'Qualifiers',isActive:false,shouldDisplay:true, link: 'promotion-condition-rewards', icon: 'fa-money'},
                 {name:'Location',isActive:false,shouldDisplay:true, link: 'promotion-location', icon: 'fa-globe'},
                 {name:'Rewards',isActive:false,shouldDisplay:true, link: 'promotion-rewards', icon: 'fa-gift'},
                 {name:'Descriptions',isActive:false,shouldDisplay:true, link: 'promotion-descriptions', icon: 'fa-pencil-square-o'},
