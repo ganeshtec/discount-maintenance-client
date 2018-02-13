b39fb36ca9424bf0156e7f3908942b6a4d87b22d
Patrick Woods and Spandan Amanaganti <pair+Patrick+Spandan@pivotallabs.com>
2018-02-13 13:35:36 -0500

    Fix ESLint and Unit tests.

--- test/qualifier/customerSegmentDataService.spec.js
+++ test/qualifier/customerSegmentDataService.spec.js
@@ -6,10 +6,16 @@ describe('checking Customer Segment Data Service for 200 response code', functio
         $httpBackend;
 
 
-    it('Test a Data Service call ', inject(function ($httpBackend) {
-
+    it('Test Customer Segments Data Service call for V1 Endpoint', inject(function ($httpBackend) {
         var url = 'http://promotionsadminsku.apps-np.homedepot.com/v1/customersegment/segments';
-        $httpBackend.when('POST', url, function () {
+        $httpBackend.when('GET', url, function () {
+            return true;
+        }).respond(200, true);
+    }));
+
+    it('Test Customer Segments Data Service call for V2 Endpoint ', inject(function ($httpBackend) {
+        var url = 'http://promotionsadminsku.apps-np.homedepot.com/v1/customersegment/v2/segments';
+        $httpBackend.when('GET', url, function () {
             return true;
         }).respond(200, true);
     }));
