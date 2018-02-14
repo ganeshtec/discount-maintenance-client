3a39fbc0fbdfeb51247e8db40a12c4e2fcf2825e
Patrick Woods and Spandan Amanaganti <pair+Patrick+Spandan@pivotallabs.com>
2018-02-14 10:04:22 -0500

    Fix Eslint.

--- env_config/prod/urls.js
+++ env_config/prod/urls.js
@@ -1,9 +1,9 @@
 app.factory('URL_CONFIG', [function() {
     var _construct = function URL_CONFIG() {
         var data = {
-            "dashboardUiUrl": "https://discount-engine.apps-zb.homedepot.com",
-            "serviceUrl": "https://promotionsws-ext.apps-zb.homedepot.com/v1",
-            "authorizeUrl" : "https://promotionsws-ext.apps-zb.homedepot.com/v1/authorization/isAuthorized/",
+            'dashboardUiUrl': 'https://discount-engine.apps-zb.homedepot.com',
+            'serviceUrl': 'https://promotionsws-ext.apps-zb.homedepot.com/v1',
+            'authorizeUrl' : 'https://promotionsws-ext.apps-zb.homedepot.com/v1/authorization/isAuthorized/',
         };
         return data;
     };
