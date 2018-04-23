31d540a0248aaca63fb14bc640c1ac8a9db33aad
Spandan Amanaganti <spandan_amanaganti@homedepot.com>
2018-04-23 16:27:17 -0400

    Reverting url back

--- env_config/local/urls.js
+++ env_config/local/urls.js
@@ -2,8 +2,8 @@ app.factory('URL_CONFIG', [function(){
     var _construct = function URL_CONFIG(){
         var data = {
             'dashboardUiUrl': 'http://localhost.homedepot.com:8002',
-            //'serviceUrl' : 'https://promotionsws-ext-ad.apps-np.homedepot.com/v1',
-            'serviceUrl' : 'http://localhost.homedepot.com:8080/v1',
+            'serviceUrl' : 'https://promotionsws-ext-ad.apps-np.homedepot.com/v1',
+            // 'serviceUrl' : 'http://localhost.homedepot.com:8080/v1',
             'authorizeUrl': 'https://promotionsws-ext-ad.apps-np.homedepot.com/v1/authorization/isAuthorized/'
         };
         return data;
