b8b346ca5667c8136d0571c989bf3a3596c708ff
Spandan Amanaganti <spandan_amanaganti@homedepot.com>
2018-04-26 11:32:43 -0400

    Modified Test for dcm cust segment

--- test/PromotionAdmin/PromotionAdminCtrl.spec.js
+++ test/PromotionAdmin/PromotionAdminCtrl.spec.js
@@ -64,7 +64,7 @@ describe('promotionAdminCtrl', function () {
                 displayPrintLabel: false,
                 displayItemsSku:false,
                 displayMerchHiearchy: false,
-                displayCustomerSegment: false,
+                displayCustomerSegment: true,
                 promotionSubTypesForMFA: false,
                 displayFilterSkuTypes: false,
                 displayBasketThreshold: false,
